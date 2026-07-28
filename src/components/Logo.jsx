67b1cf82db1e49516812500be44dd5025b41edac
abhinavsingh-hub <abhinavsinghdhakrekm@gmail.com>
2026-07-29 05:11:55 +0530

    logo location path fixed

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo({ className = "h-12 sm:h-14 w-auto" }) {
   return (
     <div className="flex items-center select-none">
       <img 
-        src="/assets/logo.png" 
+        src="./public/assets/logo.png" 
         alt="VRaise Ventures Logo" 
         className={`${className} object-contain mix-blend-multiply`} 
       />
